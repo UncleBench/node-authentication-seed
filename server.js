f6bab5f3a6cfabc51a890fe65d97ebfac755acc0
LAG Valentin Buergler <Valentin.Buergler@leuchterag.ch>
2019-09-06 14:00:28 +0200

    completed basic auth exercise

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const basicAuth = require('express-basic-auth');
 
 function main() {
   let server = express();
 
-  // TODO: Basic Auth
-
   const port = process.env.PORT || 8000;
   server.listen(port, () => console.log(`Server is listening on port: ${port}`));
+
+  server.use(basicAuth({
+    users: { 'admin': 'supersecret' },
+    challenge: true
+  }));
+
+  server.get("/topsecret", function(req, res) {
+    res.send(`Hoi ${req.auth.user}`);
+  });
 }
 
 main();
